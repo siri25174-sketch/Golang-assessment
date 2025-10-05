1adc3c54edbc828cc13e88e3956f778eb868d41a
siri25174-sketch <siri25174@gmail.com>
2025-10-05 03:12:39 -0400

    Added exercise2.

--- exercise2.go
+++ exercise2.go
@@ -25,4 +25,17 @@ func main() {
 	fmt.Println("Sum of numbers:", sum)
 
 	//max and min values
+	max := s[0] //for max value
+	min := s[0] //for min value
+	for _, value := range s {
+		if value > max {
+			max = value //if the value is greater than max it will update the max value
+		}
+		if value < min {
+			min = value //if the value is less than min it will update the min value
+		}
+	}
+
+	fmt.Println("Maximum value:", max)
+	fmt.Println("Minimum value:", min)
 }
