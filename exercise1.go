475d6a0551c6786ee8c9fedba94527307a2901e2
siri25174-sketch <siri25174@gmail.com>
2025-10-05 19:21:34 -0400

    Completed Exercise1.

--- exercise1.go
+++ exercise1.go
@@ -12,9 +12,9 @@ func main() {
 	//Also print whether each number is even or odd.
 	for i := 1; i <= 20; i++ {
 		if i%2 == 0 {
-			fmt.Printf("%d:even\n ", i) // this is to specify the number is even wit the place holder integer
+			fmt.Println(i, ":even")
 		} else {
-			fmt.Printf("%d:odd\n", i) // this is to  return the odd number with the place holder integer
+			fmt.Println(i, ":odd")
 		}
 	}
 }
